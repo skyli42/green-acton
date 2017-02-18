b8d23e9b5f120b7418e15fb46379067012d7626d
Richard <richard1248c@gmail.com>
2017-02-17 23:29:18 -0500

    map.js
    js file for map, dataset, actions and stuff

---
+++ map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JlZW5hY3RvbiIsImEiOiJjaXlobXFxdTYwNXpuMzJvZTkyaHZkY3FnIn0.eTMHeFb7rAlnxU08juWXzQ';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v9',
+	center: [-71.432, 42.482],
+	zoom: 13
+});
+
+
