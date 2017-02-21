8f1e99713a2744ec0fdd3d3a9f108e9d84438c04
Richard <richard1248c@gmail.com>
2017-02-21 15:36:01 -0500

    Acton
    Acton

--- map.js
+++ map.js
@@ -5,7 +5,8 @@ var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v9',
 	center: [-71.432, 42.482],
-	zoom: 13
+	zoom: 13,
+	maxBounds: [[-71.5, 42.429], [-71.375, 42.54]],
 });
 
 //access token: sk.eyJ1IjoiZ3JlZW5hY3RvbiIsImEiOiJjaXpiaGkyM3cwcGY1MnhxcHhhZjlpeTZiIn0.cL48iVWM8qYJG6rroRBrow
