ce7fd58b1d2b370b03037f14f1ee22d16d2c36d9
Jim Snyder-Grant <jimsg@newview.org>
2017-03-16 15:03:40 -0400

    testing dataset write on backend

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var app = express()
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
-var client = new MapboxClient('pk.eyJ1IjoiZ3JlZW5hY3RvbiIsImEiOiJjaXlobXFxdTYwNXpuMzJvZTkyaHZkY3FnIn0.eTMHeFb7rAlnxU08juWXzQ');
+// sk... can access dataset write
+var client = new MapboxClient('sk.eyJ1IjoiZ3JlZW5hY3RvbiIsImEiOiJjaXpiaGkyM3cwcGY1MnhxcHhhZjlpeTZiIn0.cL48iVWM8qYJG6rroRBrow');
 
 app.use(express.static("./assets"));
 
@@ -22,8 +23,22 @@ io.on('connection', function(socket){
 		console.log("name: " + data.name);
 		console.log("email address: " + data.emailAddress);
 		console.log("segments: " + data.featureIds);
+		// step 1: verify we can do dataset writes in backend 
+        // 1A) get a feature to work with        
+        client.readFeature('cff800c8b0ddafc54950d67776cf8153', 'cj05n0i9p0ma631qltnyigi85', function (err, feature) {
+            if (err) console.log(err);
+            console.log("feature id: " + feature.id);
+            console.log("current state: " + feature.properties.state);
+            // 1B) make a change
+            feature.properties.state = (1+feature.properties.state) % 3; //increment & wrap
+            console.log("proposed state: " + feature.properties.state);
+            // 1C) write it back
+            client.insertFeature(feature, 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
+                if (err) console.log(err);
+            });
+        });
 	});
-})
+});
 
 server.listen(app.listen(process.env.PORT || 3000, function() {
 	var host = server.address().address;
@@ -35,4 +50,4 @@ server.listen(app.listen(process.env.PORT || 3000, function() {
 //     if (err) console.log(err);
 //     // console.log(feature);
 // });
-var url = process.env.MONGOLAB_URL; //environment variable
+// var url = process.env.MONGOLAB_URL; //environment variable
