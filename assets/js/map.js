3826c098b5238a05dc5d5496bf68496bf57cefbf
Sky Li <skylimail42@gmail.com>
2017-03-17 20:19:12 -0400

    databasing

--- assets/js/map.js
+++ assets/js/map.js
@@ -9,11 +9,14 @@ var socket = io.connect("http://localhost:3000");
 // var tileset = 'acton-streets';
 
 var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/greenacton/ciyhnkn57002n2ro46orppg16',
-        center: [-71.432, 42.482],
-        zoom: 13,
-        maxBounds: [[-71.5, 42.429], [-71.375, 42.54]],
+    container: 'map',
+    style: 'mapbox://styles/greenacton/ciyhnkn57002n2ro46orppg16',
+    center: [-71.432, 42.482],
+    zoom: 13,
+    maxBounds: [
+        [-71.5, 42.429],
+        [-71.375, 42.54]
+    ],
 });
 
 // Create a popup, but don't add it to the map yet.
@@ -25,103 +28,118 @@ var curFeatureIds = [];
 var curStreetNames = [];
 
 map.on('mousemove', function(e) {
-        var bbox = [[e.point.x - 8, e.point.y - 8], [e.point.x + 8, e.point.y + 8]];
-        var features = map.queryRenderedFeatures(bbox, {layers:['acton-segments']});
-        
-        if (features.length){
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = features.length ? 'pointer' : '';
-
-   
-            var feature = features[0];
-            // Populate the popup and set its text
-            // based on the feature found. Put it in the middle of the road's coordinates
-            popup.setLngLat(feature.geometry.coordinates[Math.floor(feature.geometry.coordinates.length/2)])
-                .setText(feature.properties.street + ' between ' 
-                + ((feature.properties.start == null) ? 'end of the road' : feature.properties.start) + ' and ' 
-                + ((feature.properties.end == null) ? 'end of the road' : feature.properties.end ))
-                .addTo(map);
-         }
+    var bbox = [
+        [e.point.x - 8, e.point.y - 8],
+        [e.point.x + 8, e.point.y + 8]
+    ];
+    var features = map.queryRenderedFeatures(bbox, {
+        layers: ['acton-segments']
     });
 
-map.on('click', function(e) {
-        // set bbox as 8px reactangle area around clicked point
-        var bbox = [[e.point.x - 8, e.point.y - 8], [e.point.x + 8, e.point.y + 8]];
-        var features = map.queryRenderedFeatures(bbox, {layers:['acton-segments']});
-        
-        
-        //socket.emit('sendFeatures', {features});
-        for (var i = 0; i < features.length; i++){
-                if(!curFeatureIds.includes(features[i].id)){
-                    curFeatureIds.push(features[i].id);
-                    curStreetNames.push(features[i]);
-                    map.addLayer({
-                    'id': 'segment-' + curStreetNames.length,
-                    'type': 'line',
-                    'source': {
-                         'type': 'geojson',
-                         'data': features[i]
-                     },
-                    'layout': {},
-                    'paint': {
-                       'line-color': '#222',
-                       'line-opacity': 0.35,
-                       'line-width': 10
-                     }
-                    });
-                }
-        }
+    if (features.length) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
 
-	   // console.log(features);
-	   // console.log(features[0].id);
-	   
-        // Run through the selected features and set a filter
-        // to match features with unique FIPS codes to activate
-        // the `counties-highlighted` layer.
-        // var filter = features.reduce(function(memo, feature) {
-        //     memo.push(feature.properties.FIPS);
-        //     return memo;
-        // }, ['in', 'FIPS']);
 
-        // map.setFilter("counties-highlighted", filter);
-
-        // for(var i = 0; i < curStreetNames.length; i++) {
-        // console.log(curStreetNames[i]);
-        // }
+        var feature = features[0];
+        // Populate the popup and set its text
+        // based on the feature found. Put it in the middle of the road's coordinates
+        popup.setLngLat(feature.geometry.coordinates[Math.floor(feature.geometry.coordinates.length / 2)])
+            .setText(feature.properties.street + ' between ' +
+                ((feature.properties.start == null) ? 'end of the road' : feature.properties.start) + ' and ' +
+                ((feature.properties.end == null) ? 'end of the road' : feature.properties.end))
+            .addTo(map);
+    }
 });
 
-$('#form').submit(function(event){  
-        var nameInput = $('#nameInput').val();
-        var emailInput = $('#emailInput').val();
-        
-        if (nameInput == "") {
-                $('#invalidEmail').empty();
-                $('#submitted').empty();
-                $('#segments').empty();
-                $('#invalidName').html("Please enter a name.");
-        }
-        else if (!isValidEmail(emailInput)) {
-                $('#invalidName').empty();
-                $('#submitted').empty();
-                $('#segments').empty();
-                $('#invalidEmail').html("invalid email address");    
-        }
-        else {
-                socket.emit('sendInfo', {name: nameInput, emailAddress: emailInput, featureIds: curFeatureIds});
-                $('#invalidName').empty();
-                $('#invalidEmail').empty();
-                $('#submitted').html("Thanks for signing up!");
-                $('#segments').html('your current street segments:<br>');
-                for(var i = 0; i < curFeatureIds.length; i++) {
-                        $('#segments').append(curFeatureIds[i] + '<br>');
+map.on('click', function(e) {
+    // set bbox as 8px reactangle area around clicked point
+    var bbox = [
+        [e.point.x - 8, e.point.y - 8],
+        [e.point.x + 8, e.point.y + 8]
+    ];
+    var features = map.queryRenderedFeatures(bbox, {
+        layers: ['acton-segments']
+    });
+
+
+    //socket.emit('sendFeatures', {features});
+    for (var i = 0; i < features.length; i++) {
+        console.log(features[i])
+        var idToSend = features[i].properties.street + "" + features[i].properties.id; //[streetname][#]
+        // if(!curFeatureIds.includes(features[i].id)){
+        //     curFeatureIds.push(features[i].id);
+        //     curStreetNames.push(features[i]);
+        if (!curFeatureIds.includes(idToSend)) {
+            curFeatureIds.push(idToSend);
+            curStreetNames.push(features[i]);
+            map.addLayer({
+                'id': 'segment-' + curStreetNames.length,
+                'type': 'line',
+                'source': {
+                    'type': 'geojson',
+                    'data': features[i]
+                },
+                'layout': {},
+                'paint': {
+                    'line-color': '#222',
+                    'line-opacity': 0.35,
+                    'line-width': 10
                 }
+            });
         }
-        event.preventDefault();
+    }
+
+    // console.log(features);
+    // console.log(features[0].id);
+
+    // Run through the selected features and set a filter
+    // to match features with unique FIPS codes to activate
+    // the `counties-highlighted` layer.
+    // var filter = features.reduce(function(memo, feature) {
+    //     memo.push(feature.properties.FIPS);
+    //     return memo;
+    // }, ['in', 'FIPS']);
+
+    // map.setFilter("counties-highlighted", filter);
+
+    // for(var i = 0; i < curStreetNames.length; i++) {
+    // console.log(curStreetNames[i]);
+    // }
+});
+
+$('#form').submit(function(event) {
+    var nameInput = $('#nameInput').val();
+    var emailInput = $('#emailInput').val();
+
+    if (nameInput == "") {
+        $('#invalidEmail').empty();
+        $('#submitted').empty();
+        $('#segments').empty();
+        $('#invalidName').html("Please enter a name.");
+    } else if (!isValidEmail(emailInput)) {
+        $('#invalidName').empty();
+        $('#submitted').empty();
+        $('#segments').empty();
+        $('#invalidEmail').html("invalid email address");
+    } else {
+        socket.emit('sendInfo', {
+            name: nameInput,
+            emailAddress: emailInput,
+            featureIds: curFeatureIds
+        });
+        $('#invalidName').empty();
+        $('#invalidEmail').empty();
+        $('#submitted').html("Thanks for signing up!");
+        $('#segments').html('your current street segments:<br>');
+        for (var i = 0; i < curFeatureIds.length; i++) {
+            $('#segments').append(curFeatureIds[i] + '<br>');
+        }
+    }
+    event.preventDefault();
 })
 
-function isValidEmail(emailAddress)   
-{ 
+function isValidEmail(emailAddress) {
     var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regEx.test(emailAddress);
 }
-
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var MongoClient = require('mongodb').MongoClient
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = require("bluebird")
+    // var url = process.env.MONGOLAB_URL; //url (will be set on heroku, needs to be set on desktop systems)
+var url = "mongodb://greenacton:350PPMofCO2@ds157549.mlab.com:57549/green-acton";
 var MapboxClient = require('mapbox');
 var express = require('express')
 var http = require('http');
@@ -13,38 +18,60 @@ var client = new MapboxClient('sk.eyJ1IjoiZ3JlZW5hY3RvbiIsImEiOiJjaXpiaGkyM3cwcG
 
 app.use(express.static("./assets"));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-
-	socket.on('sendInfo', function(data){
-		console.log("name: " + data.name);
-		console.log("email address: " + data.emailAddress);
-		console.log("segments: " + data.featureIds);
-		// step 1: verify we can do dataset writes in backend 
-        // 1A) get a feature to work with        
-        client.readFeature('cff800c8b0ddafc54950d67776cf8153', 'cj05n0i9p0ma631qltnyigi85', function (err, feature) {
-            if (err) console.log(err);
-            console.log("feature id: " + feature.id);
-            console.log("current state: " + feature.properties.state);
-            // 1B) make a change
-            feature.properties.state = (1+feature.properties.state) % 3; //increment & wrap
-            console.log("proposed state: " + feature.properties.state);
-            // 1C) write it back
-            client.insertFeature(feature, 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
-                if (err) console.log(err);
-            });
-        });
-	});
+var idSchema = new Schema({
+    name: String,
+    id: String
 });
+var ID = mongoose.model('id', idSchema);
+
+mongoose.connect(url).then(function() {
+    console.log("connected to mongo database")
+    io.on('connection', function(socket) {
+
+        socket.on('sendInfo', function(data) {
+
+            //query for mapbox id
+            for(var i in data.featureIds){
+                ID.find({name:data.featureIds[i]}).select('id').then(function(id, err){
+                    if(err)console.log(err);
+                    console.log(data.featureIds[i])
+                    console.log(id[0].id);
+                })
+            }
+            // console.log("name: " + data.name);
+            // console.log("email address: " + data.emailAddress);
+            // console.log("segments: " + data.featureIds);
+
+
+            // // step 1: verify we can do dataset writes in backend 
+            // // 1A) get a feature to work with
+            // client.readFeature('cff800c8b0ddafc54950d67776cf8153', 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
+            //     if (err) console.log(err);
+            //     console.log("feature id: " + feature.id);
+            //     console.log("current state: " + feature.properties.state);
+            //     // 1B) make a change
+            //     feature.properties.state = (1 + feature.properties.state) % 3; //increment & wrap
+            //     console.log("proposed state: " + feature.properties.state);
+            //     // 1C) write it back
+            //     client.insertFeature(feature, 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
+            //         if (err) console.log(err);
+            //     });
+            // });
+        });
+    });
+
+})
 
 server.listen(app.listen(process.env.PORT || 3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 }));
 
+console.log("Listening on port 3000")
 
 // client.readFeature('000d146ae23ead63062756703852513a', 'ciyhnerdx05c92wl4wogi3hdg', function (err, feature) {
 //     if (err) console.log(err);
