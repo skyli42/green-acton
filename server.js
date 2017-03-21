a8815a4b5dded80668494e34d70d7b6c236cde62
Richard <richard1248c@gmail.com>
2017-03-21 16:43:43 -0400

    register page?

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ var ID = mongoose.model('id', idSchema);
 
 mongoose.connect(url).then(function() {
     console.log("connected to mongo database")
+  
     io.on('connection', function(socket) {
         socket.on('registration', function(data) {
             console.log("arrived");
@@ -39,44 +40,45 @@ mongoose.connect(url).then(function() {
             console.log("phone number: " +  data.phoneNumber);
             console.log("group number: " + data.groupNumber);
         });
-        //socket.on('sendInfo', function(data) {
 
-            //query for mapbox id
-            // for(var i in data.featureIds){
-            //     ID.find({name:data.featureIds[i]}).select('id name').then(function(row, err){
+        socket.on('sendInfo', function(data) {
+
+            query for mapbox id
+            for(var i in data.featureIds){
+                ID.find({name:data.featureIds[i]}).select('id name').then(function(row, err){
                     
-            //         if(err)console.log(err);
-            //         client.readFeature(row[0].id, dataset_id, function(err, feature) {
-            //             if (err) console.log(err);
-            //             feature.properties.state = data.newState;
-            //             feature.properties.claimedby = data.emailAddress;
-            //             client.insertFeature(feature, dataset_id, function(err, feature) {
-            //                 if (err) console.log(err);
-            //                 console.log(feature.properties);
-            //             })
-            //         })
-            //     })
-            // }
-            // console.log("name: " + data.name);
-            // console.log("email address: " + data.emailAddress);
-            // console.log("segments: " + data.featureIds);
+                    if(err)console.log(err);
+                    client.readFeature(row[0].id, dataset_id, function(err, feature) {
+                        if (err) console.log(err);
+                        feature.properties.state = data.newState;
+                        feature.properties.claimedby = data.emailAddress;
+                        client.insertFeature(feature, dataset_id, function(err, feature) {
+                            if (err) console.log(err);
+                            console.log(feature.properties);
+                        })
+                    })
+                })
+            }
+            console.log("name: " + data.name);
+            console.log("email address: " + data.emailAddress);
+            console.log("segments: " + data.featureIds);
 
 
-            // // step 1: verify we can do dataset writes in backend 
-            // // 1A) get a feature to work with
-            // client.readFeature('cff800c8b0ddafc54950d67776cf8153', 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
-            //     if (err) console.log(err);
-            //     console.log("feature id: " + feature.id);
-            //     console.log("current state: " + feature.properties.state);
-            //     // 1B) make a change
-            //     feature.properties.state = (1 + feature.properties.state) % 3; //increment & wrap
-            //     console.log("proposed state: " + feature.properties.state);
-            //     // 1C) write it back
-            //     client.insertFeature(feature, 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
-            //         if (err) console.log(err);
-            //     });
-            // });
-        //});
+            // step 1: verify we can do dataset writes in backend 
+            // 1A) get a feature to work with
+            client.readFeature('cff800c8b0ddafc54950d67776cf8153', 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
+                if (err) console.log(err);
+                console.log("feature id: " + feature.id);
+                console.log("current state: " + feature.properties.state);
+                // 1B) make a change
+                feature.properties.state = (1 + feature.properties.state) % 3; //increment & wrap
+                console.log("proposed state: " + feature.properties.state);
+                // 1C) write it back
+                client.insertFeature(feature, 'cj05n0i9p0ma631qltnyigi85', function(err, feature) {
+                    if (err) console.log(err);
+                });
+            });
+        });
 
 
 
