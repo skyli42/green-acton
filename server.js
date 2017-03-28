726ab402d43bd5a9015b829eb0fe9da3a16abc7f
Jim Snyder-Grant <jimsg@newview.org>
2017-03-28 02:55:39 -0400

    progress towards dataset-> tileset connection

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ var io = require('socket.io').listen(server);
 var dataset_id = 'cj05n0i9p0ma631qltnyigi85'  // id for segments
 
 
-
-///  s3.putObject({
-//    Bucket: credentials.bucket,
-//    Key: credentials.key,
-//    Body: fs.createReadStream('/path/to/file.mbtiles')
-//  }, function(err, resp) {
-//  });
-// );
-
 app.use(express.static("./assets"));
 
 app.get('/', function(req, res) {
@@ -80,9 +71,7 @@ mongoose.connect(url).then(function() {
             console.log("segments: " + data.featureIds);
         });
     });
-    // regSocket.on('connection', function(socket){
-    //     console.log("s")
-    // })
+ 
 })
 
 server.listen(app.listen(port, function() {
@@ -91,6 +80,50 @@ server.listen(app.listen(port, function() {
 }));
 
 console.log("Listening on port " + port)
+
+var timeDatasetLastUpdated = 0;
+var TileSetInProcess=0;
+
+const Readable = require('stream').Readable;
+
+var datasetProperties;
+
+class ReadableDataset extends Readable {
+  constructor(opt) {
+    super(opt);
+    this._max = 10;
+    this._index = 1;
+    this.length = 11;    //we need to more work to set this value propely
+    client.readDataset(dataset_id,
+    function(err, dataset_data) {
+        if (err) {console.log('dataset read error: ' + err );}
+        console.log('dataset data: ' + JSON.stringify(dataset_data) );
+
+        datasetProperties = dataset_data;
+  });
+  }
+ 
+ byteLength(){
+    return datasetProperties.size;
+ }
+  
+  _read() {
+    console.log('in_read');
+    var i = this._index++;
+    if (i > this._max) {
+      console.log('_read is done ');
+      this.push(null);
+    }
+    else {
+      var str = '' + i;
+      console.log('_read str is ' + str);
+      var buf = Buffer.from(str, 'ascii');
+      this.push(buf);
+    }
+  }
+}
+
+
 
 
 client.createUploadCredentials(function(err, credentials) {
@@ -101,7 +134,19 @@ client.createUploadCredentials(function(err, credentials) {
        sessionToken: credentials.sessionToken,
        region: 'us-east-1'});
     console.log('From S3: ' + credentials.accessKeyId);
+    var datasetReader = new ReadableDataset();
     
-  });
-
+    s3.putObject({
+    Bucket: credentials.bucket,
+    Key: credentials.key,
+   Body: datasetReader
+  }, function(err, resp) {
+        if (err) {
+            console.log("Error ", err);
+        } 
+        if (resp) {
+            console.log("Upload Success ", resp);
+        }   
+     });
+});
 
