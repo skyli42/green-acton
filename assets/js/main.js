686febd944744da40d416345ebd698bb9c2b26ab
Sky Li <skylimail42@gmail.com>
2017-04-07 22:31:03 -0400

    message handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@ var socket = io();
 socket.on('message', function(msg){
 	Materialize.toast(messages.myMessages.properties[msg].msg_string, 4000)
     // console.log("message: " + msg + " handler: " + localMessageHandler)
-    // localMessageHandler && localMessageHandler(msg) //commented out for now
+    localMessageHandler && localMessageHandler(msg)
 })
